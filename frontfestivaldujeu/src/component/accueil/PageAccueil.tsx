a603a768eab2563abb062c3995ef68f61872a524
Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
2023-11-15 11:56:29 +0100

    Navbar v1

--- frontfestivaldujeu/src/component/accueil/PageAccueil.tsx
+++ frontfestivaldujeu/src/component/accueil/PageAccueil.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
 import "../output.css"
+import Navbar from "../layout/Navbar"
 
 export const PageAccueil = () => {
     return (
         <div className="border">
+            <Navbar/>
             <Link to="/signin">
                 Connexion 
             </Link>
