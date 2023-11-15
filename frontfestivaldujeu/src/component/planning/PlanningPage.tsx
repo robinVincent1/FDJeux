ddb37635f0d5814d1f8c68071a3cf89540eee3e2
Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
2023-11-15 20:53:06 +0100

    Planning v2.1
    Possible de créer un planning a partir de rien

--- frontfestivaldujeu/src/component/planning/PlanningPage.tsx
+++ frontfestivaldujeu/src/component/planning/PlanningPage.tsx
@@ -3,6 +3,7 @@ import Planning from './Planning'
 
 export const PlanningPage = () => {
   return (
-    <div><Planning list_jours={[{id:0,nom:"Samedi",list_horaire:[[9,11],[11,14]] }, {id:1,nom:"Dimanche",list_horaire:[[9,11],[14,17]]}]}/></div>
+    <div><Planning list_jours={[]}/></div>
   )
 }
+//[{id:0,nom:"Samedi",list_horaire:[[9,11],[11,14]] }, {id:1,nom:"Dimanche",list_horaire:[[9,11],[14,17]]}]
