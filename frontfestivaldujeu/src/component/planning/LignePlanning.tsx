577e760a816cebaef4b152b588627123f6e7a692
Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
2023-11-15 18:46:40 +0100

    Planning v2
    Planning avec ajout de ligne , de jours et d'horaire par l'admin fonctionnel

--- frontfestivaldujeu/src/component/planning/LignePlanning.tsx
+++ frontfestivaldujeu/src/component/planning/LignePlanning.tsx
@@ -10,10 +10,12 @@ import Input from '@mui/material/Input';
 
 interface LigneProps {
     titre:string;
+    nb_creneaux:number;
   }
 
 const LignePlanning: React.FC<LigneProps> = ({
-    titre: initialTitre
+    titre: initialTitre,
+    nb_creneaux
 }) => {
     const [titre, setTitre] = useState<string>(initialTitre);
     const [inputValue, setInputValue] = useState<string>('');
@@ -37,10 +39,19 @@ const LignePlanning: React.FC<LigneProps> = ({
       function onclose(){
         handleClose();
       }
+      const generateCreneaux = () => {
+      const creneaux = [];
+      for (let i = 0; i < nb_creneaux; i++) {
+        creneaux.push(
+          <td key={i} className="px-6 py-4 bg-blue-500">
+            <Creneau ouvert={true} horaire="9h-11h" jour="Samedi" titre={titre} nb_max={15} nb_inscrit={8} />
+          </td>
+        );
+      }
+      return creneaux;
+    };
     
   return (
-    <div>
-        <table>
         <tr className="bg-blue-600 border-b border-blue-400">
     <th scope="row" className="bg-blue-600 border-b border-blue-400">
     <Button onClick={handleOpen}>{titre}</Button>
@@ -72,19 +83,9 @@ const LignePlanning: React.FC<LigneProps> = ({
                 </Button>
         </ModalDialog>
       </Modal>
-        </th>
-    <td className="px-6 py-4 bg-blue-500"><Creneau  ouvert={true} horaire="9h-11h" jour="Samedi" titre={titre} nb_max={15} nb_inscrit={8}/></td>
-    <td className="px-6 py-4 bg-blue-500"><Creneau  ouvert={true} horaire="11h-14h" jour="Samedi" titre={titre} nb_max={15} nb_inscrit={8}/></td>
-    <td className="px-6 py-4 bg-blue-500"> <Creneau  ouvert={true} horaire="14h-17h" jour="Samedi" titre={titre} nb_max={15} nb_inscrit={8}/></td>
-    <td className="px-6 py-4 bg-blue-500"><Creneau  ouvert={true} horaire="17h-20h" jour="Samedi" titre={titre} nb_max={15} nb_inscrit={8}/></td>
-    <td className="px-6 py-4 bg-blue-500"><Creneau  ouvert={true} horaire="20h-22h" jour="Samedi" titre={titre} nb_max={15} nb_inscrit={8}/></td>
-    <td className="px-6 py-4"><Creneau  ouvert={true} horaire="9h-11h" jour="Dimanche" titre={titre} nb_max={15} nb_inscrit={8}/></td>
-    <td className="px-6 py-4"><Creneau  ouvert={true} horaire="11h-14h" jour="Dimanche" titre={titre} nb_max={15} nb_inscrit={8}/></td>
-    <td className="px-6 py-4"><Creneau  ouvert={true} horaire="14h-17h" jour="Dimanche" titre={titre} nb_max={15} nb_inscrit={8}/></td>
-    <td className="px-6 py-4"> <Creneau  ouvert={true} horaire="17h-20h" jour="Dimanche" titre={titre} nb_max={15} nb_inscrit={8}/></td>
+      </th>
+        {generateCreneaux()}
     </tr>
-  </table>
-    </div>
   )
 }
 
