9425449c1e1efa6fded4ecefdfdacad6be2ab9c4
Robin Vincent <robin.vincent@etu.umontpellier.fr>
2023-11-15 13:51:47 +0100

    page admin V2

--- frontfestivaldujeu/src/component/admin/AdminPage.tsx
+++ frontfestivaldujeu/src/component/admin/AdminPage.tsx
@@ -12,25 +12,96 @@ export type User = {
 
 }
 
-const robin : User = {
+const lucas : User = {
     id: "1idzhcdzvch",
-    name: "Robin Vincent",
+    name: "Lucas",
     email: "robin.vin100@gmail.com",
     password: "kcndnc",
     image: "jcdsc",
     pseudo: "robinvincent",
-    role: "Admin"
+    role: "Référent"
+}
+
+const robin : User = {
+  id: "1idzhcdzvch",
+  name: "Robin Vincent",
+  email: "robin.vin100@gmail.com",
+  password: "kcndnc",
+  image: "jcdsc",
+  pseudo: "robinvincent",
+  role: "Admin"
+}
+
+const lilian : User = {
+  id: "1idzhcdzvch",
+  name: "Lilian",
+  email: "robin.vin100@gmail.com",
+  password: "kcndnc",
+  image: "jcdsc",
+  pseudo: "robinvincent",
+  role: "Réspo soirée"
+}
+
+const samy : User = {
+  id: "1idzhcdzvch",
+  name: "Samy",
+  email: "robin.vin100@gmail.com",
+  password: "kcndnc",
+  image: "jcdsc",
+  pseudo: "robinvincent",
+  role: "Bénévole Accueil"
+}
+
+const romain : User = {
+  id: "1idzhcdzvch",
+  name: "Romain",
+  email: "robin.vin100@gmail.com",
+  password: "kcndnc",
+  image: "jcdsc",
+  pseudo: "robinvincent",
+  role: "Bénévole"
 }
 
 
 export const AdminPage = () => {
-    const [listeUser, setListeUser] = useState([robin, robin, robin, robin, robin, robin, robin, robin, robin, robin, robin, robin, robin, robin]);
-  
+    const [listeAdmin, setListeAdmin] = useState([robin, robin, robin, robin, robin, robin, robin, robin, robin, robin, robin, robin, robin, robin]);
+    const [listeReferent, setListeReferent] = useState([lucas, lucas,lucas,lucas,lucas,lucas,lucas,lucas,lucas,lucas,lucas,lucas,lucas,lucas,lucas,lucas,]);
+    const [listeRespoSoiree, setListeRespoSoiree] = useState([lilian,lilian,lilian,lilian,lilian,lilian,lilian,lilian,lilian,lilian,lilian,lilian,lilian,lilian,]);
+    const [listeAccueilBenevole, setListeAccueilBenevole] = useState([samy,samy,samy,samy,samy,samy,samy,samy,samy,samy,samy,samy,samy,samy,samy,samy,samy,samy,samy,]);
+    const [listeBenevole, setListeBenevole] = useState([romain,romain,romain,romain,romain,romain,romain,romain,romain,romain,romain,romain,romain,romain,romain,romain,]);
     return (
-      <div className="border">
-        {listeUser.map((user) => (
+      <div className=" grid grid-cols-5">
+      <div className=" ">
+        <strong className="flex justify-center  p-2 text-[red]">Admin</strong>
+        {listeAdmin.map((user) => (
           <ProfilUserModifiable key={user.id} u={user} />
         ))}
+      </div>
+      <div className="">
+        <strong className="flex justify-center  p-2 text-[red]">Référent</strong>
+        {listeReferent.map((user) => (
+          <ProfilUserModifiable key={user.id} u={user} />
+        ))}
+      </div>
+      <div className="">
+        <strong className="flex justify-center  p-2 text-[red]">Résponsable soirée </strong>
+        
+        {listeRespoSoiree.map((user) => (
+          <ProfilUserModifiable key={user.id} u={user} />
+        ))}
+      </div>
+      <div className="">
+        <strong className="flex justify-center  p-2 text-[red]">Bénévole Accueil</strong>
+        {listeAccueilBenevole.map((user) => (
+          <ProfilUserModifiable key={user.id} u={user} />
+        ))}
+      </div>
+      <div className="">
+        <strong className="flex justify-center  p-2 text-[red]">Bénévole</strong>
+        {listeBenevole.map((user) => (
+          <ProfilUserModifiable key={user.id} u={user} />
+        ))}
+      </div>
       </div>
     );
   };
--- frontfestivaldujeu/src/component/admin/ProfilUserModifiable.tsx
+++ frontfestivaldujeu/src/component/admin/ProfilUserModifiable.tsx
@@ -1,16 +1,14 @@
 import React, { useState, ChangeEvent } from "react";
 import { User } from "./AdminPage";
-import "../output.css"
-import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
-
+import "../output.css";
+import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
 
 type ProfilUserModifiableProps = {
-    u: User;
-  };
-  
+  u: User;
+};
 
-export const ProfilUserModifiable = ({u}: ProfilUserModifiableProps) => {
-  const [modif, setModif] = useState(false);
+export const ProfilUserModifiable = ({ u }: ProfilUserModifiableProps) => {
+  const [modif, setModif] = useState(true);
 
   const [selectedOption, setSelectedOption] = useState("");
 
@@ -19,50 +17,51 @@ export const ProfilUserModifiable = ({u}: ProfilUserModifiableProps) => {
   };
 
   return (
-    <div className="border ">
-        <div>
-        {u.pseudo}
-        </div>
-      
-      <p>
-        <div className="">
-        {u.name}
-        </div>
-        <div>
-        {u.email}
-        </div>
-        {modif ? (
-          <button
-            onClick={() => {
-              return setModif(false);
-            }}
-          >
-             Role : <strong> {u.role}</strong>
-          </button>
-        ) : (
-          <div>
-            <label htmlFor="dropdown">Role :</label>
-            <select
-              id="dropdown"
-              value={selectedOption}
-              onChange={handleSelectChange}
-            >
-              <option value="Admin">Admin</option>
-              <option value="Référent">Référent</option>
-              <option value="Responsable soirée découverte">
-                Résponsable soirée découverte
-              </option>
-              <option value="Accueil bénévole">Accueil bénévole</option>
-              <option value="Bénévole">Bénévole</option>
-            </select>
+    <div className="p-2">
+      <div className="  flex justify-center shadow-xl rounded-lg ">
+        <div className="p-4">
+          <div className="text-md font-bold">{u.name}</div>
+          <div className=" text-sm">{u.pseudo}</div>
+          <div className="text-sm">{u.email}</div>
+          {modif ? (
             <button
-            onClick={() => {
-                return setModif(true);
-            }}
-            > <CheckCircleOutlineRoundedIcon /></button>
-          </div>
-        )}
-      </p>
+              className="border rounded p-2"
+              onClick={() => {
+                return setModif(false);
+              }}
+            >
+              <strong> {u.role}</strong>
+            </button>
+          ) : (
+            <div>
+              <label htmlFor="dropdown"></label>
+              <select
+                className="text-xs"
+                id="dropdown"
+                value={selectedOption}
+                onChange={handleSelectChange}
+              >
+                <option value="Admin">Admin</option>
+                <option value="Référent">Référent</option>
+                <option value="Réspo soirée">
+                  Réspo soirée
+                </option>
+                <option value="Accueil bénévole">Accueil bénévole</option>
+                <option value="Bénévole">Bénévole</option>
+              </select>
+              <button
+                className="pl-2"
+                onClick={() => {
+                  return setModif(true);
+                }}
+              >
+                {" "}
+                <CheckCircleOutlineRoundedIcon />
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
