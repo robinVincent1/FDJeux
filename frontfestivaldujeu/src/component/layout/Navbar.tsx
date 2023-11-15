1b40be72ebb506e8a49030aaed92c0fa79375513
Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
2023-11-15 12:21:17 +0100

    Navbar v1.2

--- frontfestivaldujeu/src/component/layout/Navbar.tsx
+++ frontfestivaldujeu/src/component/layout/Navbar.tsx
@@ -44,24 +44,7 @@ function Navbar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
+        <img src="/logo_FDJ_FINAL.svg"/>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
@@ -129,7 +112,7 @@ function Navbar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar src="/profil-picture.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
