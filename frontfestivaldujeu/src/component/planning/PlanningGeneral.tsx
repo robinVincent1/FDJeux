d4cc19025f81eb1636b66491d8ec5977b7562259
Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
2024-02-04 23:06:41 +0100

    Update PlanningGeneral.tsx

--- frontfestivaldujeu/src/component/planning/PlanningGeneral.tsx
+++ frontfestivaldujeu/src/component/planning/PlanningGeneral.tsx
@@ -1018,7 +1018,7 @@ const PlanningGeneral: React.FC<PlanningGeneralProps> = ({ PlanningId }) => {
                       onClick={handleOpenModal_Jour}
                       className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
                     >
-                      +
+                      →
                     </Button>
                   )}
                   <Modal
@@ -1169,7 +1169,7 @@ const PlanningGeneral: React.FC<PlanningGeneralProps> = ({ PlanningId }) => {
                   onClick={handleOpenModal_Ligne}
                   className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
                 >
-                  +
+                  ↓
                 </Button>
               )}
               <Modal
